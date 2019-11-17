3697e3af1c653cd0aaa9e14e45753fb5ab30c0b0
gavhanesneha <57831055+gavhanesneha@users.noreply.github.com>
2019-11-17 08:33:06 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,6 +9,6 @@ class HelloWorld
     { 
         System.out.println("Hello World,"); 
        System.out.println("Hello World NT,");
-      System.out.println("Hello World By Imrankhan,");
+      System.out.println("Hello everyone,");
     } 
 } 
